d61b6d0e30ce9395f9f295c8f8e041e29cc77a23
vamsimeesala789@gmail.com <vamsimeesala789@gmail.com>
2024-02-11 15:12:54 +0530

    initial changes

--- my-app/index.js
+++ my-app/index.js
@@ -12,8 +12,12 @@ let db = null
 
 const app = express()
 
+const corsOptions={
+    origin:'http://localhost:3000'
+}
+
 app.use(express.json())
-app.use(cors())
+app.use(cors(corsOptions))
 
 const initializeDatabase= async()=>{
     db = await open({
